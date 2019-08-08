44352e4e56b6ea6b3cd8779c5992877509a5d721
Richard98PL <41301282+Richard98PL@users.noreply.github.com>
2019-08-08 16:55:55 +0200

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace midnight
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Enabled = true;
+            label1.Text = DateTime.Now.ToString("HH:mm:ss");
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Text = DateTime.Now.ToString("HH:mm:ss");
+            if(label1.Text.Equals("23:59:00")) Process.Start("shutdown", "/s /t 0");
+        }
+
+        private void Label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
